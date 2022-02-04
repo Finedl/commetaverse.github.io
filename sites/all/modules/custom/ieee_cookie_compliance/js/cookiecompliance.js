47252818ac5c32a1ab6c785f664e8796d10a8c6d
finedl <936418030@qq.com>
2022-02-04 21:53:41 +0800

    web

---
+++ sites/all/modules/custom/ieee_cookie_compliance/js/cookiecompliance.js
@@ -0,0 +1,11 @@
+(function($) {
+
+  Drupal.behaviors.cookieCompliance = {
+    attach: function(context) {
+      $('body', context).once('cookiecomp', function() {
+        window.cookieconsent.initialise(json);
+      });
+    }
+  };
+
+}(jQuery));
